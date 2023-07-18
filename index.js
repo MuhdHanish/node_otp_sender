33865880e5e948d19d7690d93db6920e2413f93c
MuhdHanish <muhammedhanish11@gmail.com>
2023-07-18 10:38:33 +0530

    made some changes do some buxs

--- index.js
+++ index.js
@@ -1,57 +1,71 @@
 const nodemailer = require("nodemailer");
-const { promisify } = require("util");
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: senderEmail,
-    pass: senderPassword,
-  }
-});
+const sendOtpEmail = (
+  senderEmail,
+  senderPassword,
+  recipientEmail,
+  subject
+) => {
 
-const sendMailAsync = promisify(transporter.sendMail).bind(transporter);
+  return new Promise((resolve, reject) => {
+    if (
+      !senderEmail.length ||
+      !senderPassword.length ||
+      !recipientEmail.length ||
+      !subject.length
+    ) {
+      reject(
+        "Please provide valid details (senderEmail, senderPassword, recipientEmail, subject)."
+      );
+    } else {
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: senderEmail,
+          pass: senderPassword,
+        },
+      });
 
-const otpSender = async (senderEmail, senderPassword, recipientEmail, subject) => {
-  if (!senderEmail || !senderPassword || !recipientEmail || !subject) {
-    throw new Error("Please provide valid details (senderEmail, senderPassword, recipientEmail, subject).");
-  }
+      const otp = Math.floor(Math.random() * 900000) + 100000;
 
-  const otp = Math.floor(Math.random() * 900000) + 100000;
-
-  const mailOptions = {
-    from: senderEmail,
-    to: recipientEmail,
-    subject: subject,
-    html: `
-    <!DOCTYPE html>
-    <html>
-  <head>
-  <title>OTP Verification</title>
-  <style>
-    .otp-code {
-      font-size: 48px;
-      font-weight: bold;
-      color: #333;
-    }
-  </style>
-  </head>
-  <body>
+      const mailOptions = {
+        from: senderEmail,
+        to: recipientEmail,
+        subject: subject,
+        html: `
+      <!DOCTYPE html>
+      <html>
+      <head>
+      <title>OTP Verification</title>
+      <style>
+        .otp-code {
+        font-size: 48px;
+        font-weight: bold;
+        color: #333;
+        }
+        </style>
+      </head>
+      <body>
       <h1>OTP Verification</h1>
       <p>Here is your One-Time Password:</p>
       <p class="otp-code">${otp}</p>
       <p>Please use this code for verification.</p>
-  </body>
-  </html>
-    `,
-  };
+      </body>
+      </html>`,
+      };
 
-  try {
-    await sendMailAsync(mailOptions);
-    return { otp, message: "OTP sent successfully" };
-  } catch (error) {
-    throw new Error(`Failed to send OTP: ${error.message}`);
-  }
-  
+      transporter.sendMail(
+        mailOptions,
+        function (error, info) {
+          if (error) {
+            reject({ error, message: "Inavlid sender-email or password" });
+          } else {
+            resolve({ otp, message: "OTP sended successfully" });
+          }
+        }
+      );
+    }
+  });
 };
 
-module.exports = otpSender;
+module.exports = sendOtpEmail;
