26bd3732593e1b75ba76666606666f376f022cec
Ajay <aji478514@gmail.com>
2023-09-24 20:56:32 +0530

    implement retry functionality

--- index.js
+++ index.js
@@ -4,66 +4,79 @@ const sendOtpEmail = (
   senderEmail,
   senderPassword,
   recipientEmail,
-  subject
+  subject,
+  maxRetries = 3, // Number of retry attempts
+  retryDelay = 1000 // Delay between retries
 ) => {
+  return new Promise(async (resolve, reject) => {
+    let retries = 0;
+    //these errors may resolve after retry
+    const retryableErrors = [
+      "ETIMEDOUT",                  // Network: Connection timed out
+      "ECONNRESET",                 // Network: Connection reset by peer
+      "EHOSTUNREACH",               // Network: Host is unreachable
+      "DNSResolutionError",         // Network: DNS resolution issue
+      "SSLError"                    // Network: SSL/TLS handshake issue
+    ];
 
-  return new Promise((resolve, reject) => {
-    if (
-      !senderEmail.length ||
-      !senderPassword.length ||
-      !recipientEmail.length ||
-      !subject.length
-    ) {
-      reject(
-        "Please provide valid details (senderEmail, senderPassword, recipientEmail, subject)."
-      );
-    } else {
-      const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-          user: senderEmail,
-          pass: senderPassword,
-        },
-      });
+    while (retries < maxRetries) {
+      try {
+        const transporter = nodemailer.createTransport({
+          service: "gmail",
+          auth: {
+            user: senderEmail,
+            pass: senderPassword,
+          },
+        });
 
-      const otp = Math.floor(Math.random() * 900000) + 100000;
+        const otp = Math.floor(Math.random() * 900000) + 100000;
 
-      const mailOptions = {
-        from: senderEmail,
-        to: recipientEmail,
-        subject: subject,
-        html: `
-      <!DOCTYPE html>
-      <html>
-      <head>
-      <title>OTP Verification</title>
-      <style>
-        .otp-code {
-        font-size: 48px;
-        font-weight: bold;
-        color: #333;
+        const mailOptions = {
+          from: senderEmail,
+          to: recipientEmail,
+          subject: subject,
+          html: `
+            <!DOCTYPE html>
+            <html>
+            <head>
+            <title>OTP Verification</title>
+            <style>
+              .otp-code {
+                font-size: 48px;
+                font-weight: bold;
+                color: #333;
+              }
+            </style>
+            </head>
+            <body>
+            <h1>OTP Verification</h1>
+            <p>Here is your One-Time Password:</p>
+            <p class="otp-code">${otp}</p>
+            <p>Please use this code for verification.</p>
+            </body>
+            </html>`,
+        };
+
+        await transporter.sendMail(mailOptions);
+
+        resolve({ otp, message: "OTP sent successfully" });
+        return;
+      } catch (error) {
+        console.error(`Attempt ${retries + 1} failed: ${error.message}`);
+
+        retries++;
+        if (!retryableErrors.includes(error.code)) {
+          reject(`Sorry, we encountered an issue while sending the OTP email. Please try again later.`);
+          return;
         }
-        </style>
-      </head>
-      <body>
-      <h1>OTP Verification</h1>
-      <p>Here is your One-Time Password:</p>
-      <p class="otp-code">${otp}</p>
-      <p>Please use this code for verification.</p>
-      </body>
-      </html>`,
-      };
 
-      transporter.sendMail(
-        mailOptions,
-        function (error, info) {
-          if (error) {
-            reject({ error, message: "Inavlid sender-email or password" });
-          } else {
-            resolve({ otp, message: "OTP sended successfully" });
-          }
+        if (retries < maxRetries) {
+          // Wait for the delay before the next retry
+          await new Promise((resolve) => setTimeout(resolve, retryDelay));
+        } else {
+          reject(`Max retry attempts (${maxRetries}) reached. Email delivery failed.`);
         }
-      );
+      }
     }
   });
 };
